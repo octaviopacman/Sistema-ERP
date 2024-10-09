b2e529c28d7e71e6a60b6b55bd1ce042c253a61c
sebferreira <sebastianleonelgalarza@gmail.com>
2024-10-09 14:44:13 -0300

    creando controladores para obtener resultados de las tablas nuevas

--- back/src/config/db.js
+++ back/src/config/db.js
@@ -1,7 +1,6 @@
 import {Sequelize} from "sequelize";
 import pg from "pg";
 import dotenv from "dotenv";
-const {Pool} = pg;
 dotenv.config();
 
 const sequelize = new Sequelize({
@@ -17,12 +16,6 @@ const sequelize = new Sequelize({
       rejectUnauthorized: false,
     },
   }, */
-});
-export const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DATABASE,
 });
 
 /* const user = process.env.POSTGRES_USER;
